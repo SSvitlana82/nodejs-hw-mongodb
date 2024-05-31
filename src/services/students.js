4e38b24115bf1fc643e5e40bfedf65bf88b90f36
SSvitlana82 <144372279+SSvitlana82@users.noreply.github.com>
2024-05-31 13:29:34 +0300

    Initial commit

---
+++ src/services/students.js
@@ -0,0 +1,5 @@
+import { StudentsCollection } from '../db/models/student.js';
+export const getAllStudents = async () => {
+  const students = await StudentsCollection.find();
+  return students;
+};
