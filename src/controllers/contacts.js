8c98a560e2743195fa76d174de849b6dc3230b71
SSvitlana82 <144372279+SSvitlana82@users.noreply.github.com>
2024-06-08 22:23:43 +0300

    hw3

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,7 +1,10 @@
+import createHttpError from 'http-errors';
 import {
   getAllContacts,
   getContactById,
   createContact,
+  updateContact,
+  deleteContact,
 } from '../services/contacts.js';
 
 export const getContactsController = async (req, res) => {
@@ -40,4 +43,30 @@ export const createContactController = async (req, res) => {
     message: 'Successfully created a contact!',
     data: contact,
   });
+};
+export const patchContactController = async (req, res, next) => {
+  const contactId = req.params.contactId;
+
+  const result = await updateContact(contactId, req.body);
+  console.log(result);
+  if (!result) {
+    next(createHttpError(404, 'Contact not found'));
+    return;
+  }
+  res.json({
+    status: 200,
+    message: 'Successfully patched a contact!',
+    data: result.contact,
+  });
+};
+
+export const deleteContactController = async (req, res, next) => {
+  const contactId = req.params.contactId;
+  const contact = await deleteContact(contactId);
+  if (!contact) {
+    next(createHttpError(404, 'Contact not found'));
+    return;
+  }
+
+  res.status(204).send();
 };
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -17,3 +17,29 @@ export const createContact = async (payload) => {
   const contact = await contactsCollection.create(payload);
   return contact;
 };
+
+export const updateContact = async (contactId, payload, options = {}) => {
+  const rawResult = await contactsCollection.findOneAndUpdate(
+    { _id: contactId },
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+      ...options,
+    },
+  );
+
+  if (!rawResult || !rawResult.value) return null;
+  return {
+    contact: rawResult.value,
+    isnew: Boolean(rawResult?.lastErrorObject?.upserted),
+  };
+};
+
+export const deleteContact = async (contactId) => {
+  if (!mongoose.Types.ObjectId.isValid(contactId)) {
+    throw new Error('its not correct id');
+  }
+  const contact = await contactsCollection.findOneAndDelete({ _id: contactId });
+  return contact;
+};
