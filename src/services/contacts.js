50f9c568afb546b664449f83c98d72954686a05d
SSvitlana82 <144372279+SSvitlana82@users.noreply.github.com>
2024-06-14 12:26:51 +0300

    sort

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,8 +1,14 @@
 import mongoose from 'mongoose';
 import { contactsCollection } from '../db/models/contact.js';
 import { calculatePaginationData } from '../utils/calculatePaginationData.js';
+import { SORT_ORDER } from '../constants/index.js';
 
-export const getAllContacts = async ({ page = 1, perPage = 10 }) => {
+export const getAllContacts = async ({
+  page = 1,
+  perPage = 10,
+  sortOrder = SORT_ORDER.ASC,
+  sortBy = '_id',
+}) => {
   const limit = perPage;
   const skip = (page - 1) * perPage;
 
@@ -12,7 +18,11 @@ export const getAllContacts = async ({ page = 1, perPage = 10 }) => {
     .merge(contactsQuery)
     .countDocuments();
 
-  const contacts = await contactsQuery.skip(skip).limit(limit).exec();
+  const contacts = await contactsQuery
+    .skip(skip)
+    .limit(limit)
+    .sort({ [sortBy]: sortOrder })
+    .exec();
 
   const paginationData = calculatePaginationData(contactsCount, page, perPage);
 
